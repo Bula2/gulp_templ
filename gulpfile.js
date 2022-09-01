69ce5a6826416cddac61514b6bff2c6803c49963
Bula2 <bulaev123da@gmail.com>
2022-09-02 02:37:08 +0300

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -9,49 +9,61 @@ const concat = require("gulp-concat")
 const sourcemaps = require("gulp-sourcemaps")
 const autoprefixer = require("gulp-autoprefixer")
 const imagemin = require("gulp-imagemin")
-const htmlmin = require("gulp-htmlmin")
+const htmlMin = require("gulp-htmlmin")
 const size = require("gulp-size")
+const newer = require("gulp-newer")
+const pug = require("gulp-pug")
+const stylus = require("gulp-stylus")
+const ts = require("gulp-typescript")
+const coffee = require("gulp-coffee")
+const sass = require("gulp-sass")(require("sass"))
+const browserSync = require("browser-sync").create()
 
-
+// Пути
 const path = {
     html: {
-        src: "src/*.html",
+        src: ["src/*.html", "src/*.pug"],
         dest: "dist/"
     },
     styles: {
-        src: 'src/styles/**/*.less',
+        src: ['src/styles/**/*.less', 'src/styles/**/*.styl',
+            'src/styles/**/*.sass','src/styles/**/*.scss','src/styles/**/*.css'],
         dest: 'dist/css/'
     },
     scripts: {
-        src: 'src/scripts/**/*.js',
+        src: ['src/scripts/**/*.js', 'src/scripts/**/*.ts', 'src/scripts/**/*.coffee'],
         dest: 'dist/js/'
     },
     img: {
-        src: 'src/img/**/*',
+        src: 'src/img/**',
         dest: 'dist/img/'
     }
 }
 
 // Задача удаления
 function clean() {
-    return del(["dist"])
+    return del(['dist/*', '!dist/img'])
 }
 
 // Задача обработки html
 function html(){
     return gulp.src(path.html.src)
-        .pipe(htmlmin({ collapseWhitespace: true}))
+        // .pipe(pug())
+        .pipe(htmlMin({ collapseWhitespace: true}))
         .pipe(size({
             showFiles: true
         }))
         .pipe(gulp.dest(path.html.dest))
+        .pipe(browserSync.stream())
 }
 
 // Задача преобразования стилей
 function styles() {
     return gulp.src(path.styles.src)
         .pipe(sourcemaps.init())
-        .pipe(less())
+        // .pipe(sass().on('error', sass.logError))
+        // .pipe(stylus())
+        // .pipe(less())
         .pipe(autoprefixer({
             cascade: false
         }))
@@ -59,33 +71,41 @@ function styles() {
             level: 2
         }))
         .pipe(concat('main.min.css'))
-        .pipe(sourcemaps.write("./"))
+        .pipe(sourcemaps.write("."))
         .pipe(size({
             showFiles: true
         }))
         .pipe(gulp.dest(path.styles.dest))
+        .pipe(browserSync.stream())
 }
 
 // Задача для обработки скриптов
 function scripts() {
     return gulp.src(path.scripts.src)
         .pipe(sourcemaps.init())
+        // .pipe(coffee({bare:true}))
+        // .pipe(ts({
+        //     noImplicitAny: true,
+        //     outFile: 'main.min.js'
+        // }))
         .pipe(babel({
             presets: ['@babel/env']
         }))
         .pipe(uglify())
         .pipe(concat('main.min.js'))
-        .pipe(sourcemaps.write("./"))
+        .pipe(sourcemaps.write("."))
         .pipe(size({
             showFiles: true
         }))
         .pipe(gulp.dest(path.scripts.dest))
+        .pipe(browserSync.stream())
 }
 
 
 // Задача для обработки изображений
 function img() {
     return gulp.src(path.img.src)
+        .pipe(newer(path.img.dest))
         .pipe(imagemin({
             progressive: true
         }))
@@ -97,6 +117,13 @@ function img() {
 
 // Задача наблюдатель
 function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch(path.html.dest).on('change', browserSync.reload)
+    gulp.watch(path.html.src, html)
     gulp.watch(path.styles.src, styles)
     gulp.watch(path.scripts.src, scripts)
     gulp.watch(path.img.src, img)
